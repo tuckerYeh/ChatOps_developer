83f85f41b14acfa020fc7d4b20be914ba8f56733
tuckerYeh <world.yeh@gmail.com>
2023-08-08 20:29:06 +0800

    修改：     config.py

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ class DefaultConfig:
 
     PORT = 8000
     APP_ID = os.environ.get("MicrosoftAppId", "904e00ce-de5b-4d2c-9da9-523eed882806")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "93425324-8e4e-4dcf-be5c-32f316ca975e")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "x4O8Q~OmO3pqKK_knumL5JvTJyGxQyvgdQMiaaeF")
     #APP_OPENAIAPIKEY= os.environ.get("OpenAIAPIkey","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")
     APP_AZURE_OPENAIAPIKEY= os.environ.get("OPENAI_API_KEY","ad54cec485524fa590fd5ba8428505f0")
