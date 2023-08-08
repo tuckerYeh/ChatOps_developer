2c5246d1665f486207777cd9ef5152a811a4c57a
tuckerYeh <world.yeh@gmail.com>
2023-08-08 23:20:45 +0800

    修改：     .DS_Store 	修改：     bots/__init__.py 	修改：     bots/attachments_bot.py 	修改：     config.py 	app_echo_bot.py 	bots/.DS_Store 	bots/echo_bot.py

--- bots/__init__.py
+++ bots/__init__.py
@@ -1,6 +1,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+#from .echo_bot import EchoBot
+#__all__ = ["EchoBot"]
 from .attachments_bot import AttachmentsBot
 
 __all__ = ["AttachmentsBot"]
--- config.py
+++ config.py
@@ -11,4 +11,4 @@ class DefaultConfig:
     APP_ID = os.environ.get("MicrosoftAppId", "904e00ce-de5b-4d2c-9da9-523eed882806")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "x4O8Q~OmO3pqKK_knumL5JvTJyGxQyvgdQMiaaeF")
     #APP_OPENAIAPIKEY= os.environ.get("OpenAIAPIkey","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")
-    APP_AZURE_OPENAIAPIKEY= os.environ.get("OPENAI_API_KEY","ad54cec485524fa590fd5ba8428505f0")
+    APP_AZURE_OPENAIAPIKEY= os.environ.get("OPENAI_API_KEY","498e27ca0d0742b4b65f952e67714870")
