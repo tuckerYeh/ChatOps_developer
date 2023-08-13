f0a791917f8d0a8993dac84ef3d84a0a1f23c89b
tuckerYeh <world.yeh@gmail.com>
2023-08-13 19:54:20 +0800

    修改：     bots/attachments_bot.py 	修改：     config.py 	修改：     requirements.txt

--- bots/attachments_bot.py
+++ bots/attachments_bot.py
@@ -1,4 +1,5 @@
 import openai
+import tiktoken
 from botbuilder.core import ActivityHandler, MessageFactory, TurnContext, CardFactory
 from botbuilder.schema import (
     ChannelAccount,
@@ -10,73 +11,77 @@ from botbuilder.schema import (
     Activity,
     ActionTypes,
 )
-
+from botbuilder.core.memory_storage import MemoryStorage
+from botbuilder.core.conversation_state import ConversationState
 import sys
 sys.path.append("..")
 from config import DefaultConfig
 
 
-CONFIG = DefaultConfig()
+#CONFIG = DefaultConfig()
 
+
+class ConversationData:
+    def __init__(self, conversation=None):
+        self.conversation = conversation or []
 
 class AttachmentsBot(ActivityHandler):
-    """
-    Represents a bot that processes incoming activities.
-    For each user interaction, an instance of this class is created and the OnTurnAsync method is called.
-    This is a Transient lifetime service. Transient lifetime services are created
-    each time they're requested. For each Activity received, a new instance of this
-    class is created. Objects that are expensive to construct, or have a lifetime
-    beyond the single turn, should be carefully managed.
-    """
+    def __init__(self):
+        memory_storage = MemoryStorage()
+        self.conversation_state = ConversationState(memory_storage)
+        self.conversation_data_accessor = self.conversation_state.create_property("ConversationData")
 
-    async def on_members_added_activity(
-        self, members_added: [ChannelAccount], turn_context: TurnContext
-    ):
-        await self._send_welcome_message(turn_context)
 
-    #依據輸入行為執行動作
+    async def on_members_added_activity(self, members_added: [ChannelAccount], turn_context: TurnContext):
+        for member in members_added:
+            if member.id != turn_context.activity.recipient.id:
+                await turn_context.send_activity("歡迎!")
+
     async def on_message_activity(self, turn_context: TurnContext):
-        lcontext = turn_context.activity.text.split(" ")[0].lower()
-        lcontextai = turn_context.activity.text.lower()
-
-        if lcontext == "hello":
-           await turn_context.send_activity(f"您好！我是TuckerAI!")
-        #Azure openAI 3.5 testing    
-        elif lcontextai[0:7] == "hi kgis":
-            await turn_context.send_activity(f"證劵小幫手, 暫時無證劵的內部資料!")
-        else:
-
-            openai.api_type = "azure"
-            openai.api_base = "https://tucker-ai.openai.azure.com/"
-            openai.api_version = "2023-03-15-preview"
-            openai.api_key = CONFIG.APP_AZURE_OPENAIAPIKEY
-
-            last_response = ""
-            #user_input = turn_context.activity.text + "使用繁體中文回答"
-
-            user_input = turn_context.activity.text
-
-            # Initialize messages list with the system message
-            messages = [
-            {"role": "system", "content": "使用繁體中文，.\
+        openai.api_type = "azure"
+        openai.api_base = "https://tucker-ai.openai.azure.com/"
+        openai.api_version = "2023-05-15"
+        openai.api_key = "498e27ca0d0742b4b65f952e67714870"
+        system_message = {"role": "system", "content": "使用繁體中文，.\
                                         簡潔答覆，忽略禮貌用語，.\
                                         你是具有多領域專長的專家，若我提出的問題，。若我提出的問題，你目前的角色無法解決目前遇到的問題，就自行切換能解決此問題的角色以獲得最佳效果，.\
                                         回答前，請說現在是擔任什麼角色，然後繼續回答，.\
-                                        若我說幫我整理以下內容，請說'沒問題~我知道了'，然後用專業口吻，把內容重新敘述，辨識錯字，刪除贅字，重複語句，並且使上下文流暢好閱讀，抓重點並保留原意"},
-            ]
-            
-            prompt =  messages.append({"role": "user", "content": user_input})
+                                        若我說幫我整理以下內容，請說'沒問題~我知道了'，然後用專業口吻，把內容重新敘述，辨識錯字，刪除贅字，重複語句，並且使上下文流暢好閱讀，抓重點並保留原意"}
 
-            # Only send the last 5 messages to the API
-            recent_messages = messages[-5:]
+        max_response_tokens = 1024
+        token_limit = 4096
 
-            #response = openai.ChatCompletion.create(engine="tuckerai", messages=prompt, max_tokens=1024, temperature=0.6)
-            response = openai.ChatCompletion.create(engine="tuckerai", messages=recent_messages, max_tokens=1024, temperature=0.6)
-            # Append the assistant's response to the messages list
-            messages.append({"role": "assistant", "content": response['choices'][0]['message']['content'].strip()})
-            reply_msg = response['choices'][0]['message']['content']#.replace('\n','')
+        # 從對話狀態中取得對話
+        conversation_data = await self.conversation_data_accessor.get(turn_context, ConversationData)
+        conversation = conversation_data.conversation
 
-            await turn_context.send_activity(reply_msg)
+        user_input = turn_context.activity.text
+        conversation.append(system_message)
+        conversation.append({"role": "user", "content": user_input})
+
+        encoding = tiktoken.get_encoding("cl100k_base")
+        num_tokens = sum(len(encoding.encode(msg["content"])) + 4 for msg in conversation) + 2
+
+        while num_tokens + max_response_tokens >= token_limit:
+            del conversation[1]
+            num_tokens = sum(len(encoding.encode(msg["content"])) + 4 for msg in conversation) + 2
+
+        response = openai.ChatCompletion.create(
+            engine="tuckerai",
+            messages=conversation,
+            temperature=0.7,
+            max_tokens=max_response_tokens,
+        )
+
+        conversation.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
+        reply_msg = response['choices'][0]['message']['content']
+
+        # 更新對話
+        conversation_data.conversation = conversation
+        await self.conversation_state.save_changes(turn_context)
+
+        await turn_context.send_activity(reply_msg)
+
 
 
     async def _send_welcome_message(self, turn_context: TurnContext):
--- config.py
+++ config.py
@@ -7,10 +7,11 @@ import os
 class DefaultConfig:
     """ Bot Configuration """
 
-    PORT = 8000
+    PORT = 8004
     #APP_ID = os.environ.get("MicrosoftAppId", "904e00ce-de5b-4d2c-9da9-523eed882806")
     #APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "9Lg8Q~Y72jnI6tIAIk3xlEdq73DYbQEb7dEJBc-q")
     #APP_OPENAIAPIKEY= os.environ.get("OpenAIAPIkey","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")
-    APP_AZURE_OPENAIAPIKEY= os.environ.get("OPENAI_API_KEY","498e27ca0d0742b4b65f952e67714870")
+    APP_AZURE_OPENAIAPIKEY= os.environ.get("OPENAI_API_KEY","498e27ca0d0742b4b65f952e67714870") #GPT.35 
+    #APP_AZURE_OPENAIAPIKEY= os.environ.get("OPENAI_API_KEY","3d8bc480088f42658ee6e47a0fe0e9d8") #GPT4
     APP_ID = os.environ.get("MicrosoftAppId", "1a589cb1-1dbe-4f42-9c92-68c1c7e51b74")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "Ofe8Q~1NfmXsBgaE7i~DF~dy-.a2d0qfHcvwta1u")
