e4ffdaef4b6fadf21151c6192288835a09f63296
Tucker <tucker.yeh@kgi.com>
2023-08-14 11:10:18 +0800

    modified:   config.py

--- config.py
+++ config.py
@@ -7,12 +7,11 @@ import os
 class DefaultConfig:
     """ Bot Configuration """
 
-    PORT = 8000
+   PORT = 8004
 
     APP_ID = os.environ.get("MicrosoftAppId", "1a589cb1-1dbe-4f42-9c92-68c1c7e51b74")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "Ofe8Q~1NfmXsBgaE7i~DF~dy-.a2d0qfHcvwta1u")
+
+    #APP_OPENAIAPIKEY= os.environ.get("OpenAIAPIkey","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")
     APP_AZURE_OPENAIAPIBASE= os.environ.get("OPENAI_API_BASE","https://kgis-openai.openai.azure.com/")
     APP_AZURE_OPENAIAPIKEY= os.environ.get("OPENAI_API_KEY","4742123a7df64e46ba77bbe98e784ac1")
-
-
-
