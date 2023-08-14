bac9b7c4b54ff02f2c0f2b4e39511993fc2077df
Tucker <tucker.yeh@kgi.com>
2023-08-14 10:33:05 +0800

    deleted:    .github/workflows/main_tucker-ai-web-app-1.yml 	deleted:    .github/workflows/main_tucker-ai-web-app.yml 	deleted:    .github/workflows/main_tucker-web-app-001.yml 	deleted:    .github/workflows/main_tucker-web-app.yml 	modified:   bots/attachments_bot.py 	modified:   config.py

--- bots/attachments_bot.py
+++ bots/attachments_bot.py
@@ -35,7 +35,7 @@ class AttachmentsBot(ActivityHandler):
     async def on_members_added_activity(self, members_added: [ChannelAccount], turn_context: TurnContext):
         for member in members_added:
             if member.id != turn_context.activity.recipient.id:
-                await turn_context.send_activity("您好！我是TuckerAI,提供了ChatGPT的功能,只需輸入您的問題或要求,就會立即為您提供答案或解決方案!")
+                await turn_context.send_activity("您好！我是證券小幫手,提供了ChatGPT的功能,只需輸入您的問題或要求,就會立即為您提供答案或解決方案!")
 
     async def on_message_activity(self, turn_context: TurnContext):
         openai.api_type = "azure"
@@ -45,8 +45,7 @@ class AttachmentsBot(ActivityHandler):
         system_message = {"role": "system", "content": "使用繁體中文，.\
                                         簡潔答覆，忽略禮貌用語，.\
                                         你是具有多領域專長的專家，若我提出的問題，你目前的角色無法解決目前遇到的問題，就自行切換能解決此問題的角色以獲得最佳效果，.\
-                                        回答前，請說現在是擔任什麼角色，然後繼續回答，.\
-                                        若我說幫我整理以下內容，請說'沒問題~我知道了'，然後用專業口吻，把內容重新敘述，辨識錯字，刪除贅字，重複語句，並且使上下文流暢好閱讀，抓重點並保留原意"}
+                                        回答前，請說現在是擔任什麼角色，然後繼續回答。"}
 
         max_response_tokens = 1024
         token_limit = 4096
@@ -67,7 +66,7 @@ class AttachmentsBot(ActivityHandler):
             num_tokens = sum(len(encoding.encode(msg["content"])) + 4 for msg in conversation) + 2
 
         response = openai.ChatCompletion.create(
-            engine="tuckerai-gpt4",
+            engine="kgis-gpt35",
             messages=conversation,
             temperature=0.7,
             max_tokens=max_response_tokens,
@@ -84,7 +83,7 @@ class AttachmentsBot(ActivityHandler):
 
     #錯誤訊息
     async def _send_unrecognizable_message(self, turn_context: TurnContext):
-        await turn_context.send_activity(f"TuckerAI罷工中．請重新輸入")
+        await turn_context.send_activity(f"證券小幫手罷工中．請重新輸入")
         await self._display_index(turn_context)
 
 
