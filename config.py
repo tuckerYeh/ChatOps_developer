dfbbb38b0a90f2cbec613a0c33e1c04eae00d8db
tuckerYeh <world.yeh@gmail.com>
2023-08-15 21:19:06 +0800

    修改：     config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import os
 class DefaultConfig:
     """ Bot Configuration """
 
-   PORT = 8004
+    PORT = 8004
 
     APP_ID = os.environ.get("MicrosoftAppId", "1a589cb1-1dbe-4f42-9c92-68c1c7e51b74")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "Ofe8Q~1NfmXsBgaE7i~DF~dy-.a2d0qfHcvwta1u")
